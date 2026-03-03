e6b4c512070ec3b71d2b0a886fb86d0648bb37c4
jackan04 <jacob.asker@icloud.com>
2026-03-03 20:57:21 +0100

    add react-router-dom for routing and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<App />} />
+        <Route path="/posts" element={<App />} />
+        <Route path="/post/:id" element={<App />} />
+        <Route path="/posts/:postId/comments" element={<App />} />
+        <Route path="/posts/create" element={<App />} />
+        
+        
+        <Route path="auth" element={<App />}>
+          <Route path="/login" element={<App />} />
+          <Route path="/signup" element={<App />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
